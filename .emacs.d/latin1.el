750fdc81c9063cfcac2965e443c5d509497b4ff7
Nikolaus Thiel <Nikolaus.Thiel@homegate.ch>
2018-06-25 16:44:59 +0200

    Moved to FSFE

--- .emacs.d/latin1.el
+++
@@ -1,9 +0,0 @@
-;; Enable Latin-1 iso-88591
-(setq default-enable-multibyte-characters nil)
-(condition-case nil
-    (set-language-environment "Latin-1")
-  (error nil)
-  )
-
-(setq current-language-environment "Latin-1")
-
